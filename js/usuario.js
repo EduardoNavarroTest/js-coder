6ca9a37e3699f736ca5aafd917a5dec23f2d130e
eduardonavarro <eduardonavarro.test@gmail.com>
2024-01-24 21:52:12 -0500

    Mejoras en la creación de los objetos autos y usuarios

--- js/usuario.js
+++ js/usuario.js
@@ -1,13 +1,42 @@
 class Usuario {
-    constructor(id, nombre, apellido, correo, edad) {
+    constructor(id, nombre, apellido, correo, telefono, edad, direccion, test) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
+        this.telefono = telefono;
         this.edad = edad;
+        this.direccion = direccion;
         this.activo = false;
     }
 
     activarUsuario = () =>
         this.activo = true;
 }
+
+/* Variables Globales */
+const usuarios = [];
+
+/**
+ * Función con los datos iniciales que se utilizarán para crear los objetos
+ */
+function crearUsuarioDefault() {
+
+    /* Usuario default para pruebas */
+    crearUsuario(1052988, "Eduardo", "Navarro", "eduardonavarro.test@gmail.com", "3002918447", 99, "Cartagena - Colombia");
+   
+}
+
+
+/**
+ * Función que por parámetros recibe los datos de los usuarios para luego instanciar los objetos y agregarlos todos a un array
+ */
+function crearUsuario(id, nombre, apellido, correo, telefono, edad, direccion) {
+    const objUsuario = new Usuario(id, nombre, apellido, correo, telefono, edad, direccion);
+    objUsuario.activarUsuario();
+    usuarios.push(objUsuario);
+}
+
+
+crearUsuarioDefault();
+console.log(usuarios);
--- js/vehiculo.js
+++ js/vehiculo.js
@@ -1,26 +1,69 @@
 class Vehiculo {
 
-    constructor(tipo, marca, modelo, color, anio, descripcion, precio) {
+    constructor(id, tipo, marca, modelo, color, anio, descripcion, precio) {
+        this.id = id;
         this.tipo = tipo;
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
         this.anio = anio;
         this.descripcion = descripcion;
-        this.precio = precio; //Valor del alquiler por día
-        this.disponible = false;
+        this.precio = precio; //Valor en USD por día
+        this.disponible = true;
     }
 
-    abrirDisponibilidad = () =>
-        this.disponible = true;
+    modificarDisponibilidad = (disponible) => {
+        this.disponible = true
+    }
 
 }
 
-/* Creación de vehiculos (motos y autos) */
+/* Variables Globales */
+const vehiculos = [];
 
-/*Autos*/ 
-const toyotaCorolla20 = new Vehiculo("Auto", "Toyota", "Corolla", "Blanco", "2020", "Eficiencia de combustible y la confiabilidad", 100);
-toyotaCorolla20.abrirDisponibilidad();
+/**
+ * Función con los datos iniciales que se utilizarán para crear los objetos
+ */
+function crearObjetoVehiculo() {
+
+    /*CARROS*/
+    crearVehiculo(1, "Auto", "Ford", "Mustang", "Negro", "2020", "Diseño y calidad", 100);
+    crearVehiculo(2, "Auto", "Mercedes", "Benz", "Blanco", "2024", "Elegancia y estilo", 200);
+    crearVehiculo(3, "Auto", "BMW", "M8 GTR", "Blanco", "2023", "Seguridad al instante", 150);
+    crearVehiculo(4, "Auto", "Chevrolet", "Camaro", "Blanco", "2021", "Diseño y calidad", 300);
+    crearVehiculo(5, "Auto", "Ferrari", "488", "Rojo", "2019", "Elegancia y estilo", 270);
+    crearVehiculo(6, "Auto", "Tesla", "S", "Blanco", "2010", "Eficiencia de combustible y la confiabilidad", 95);
+    crearVehiculo(7, "Auto", "Lamborhini", "PENDIENTE", "Blanco", "2024", "Seguridad al instante", 125);
+    crearVehiculo(8, "Auto", "Bucati", "PENDIENTE", "Blanco", "2023", "Eficiencia de combustible y la confiabilidad", 75);
+    crearVehiculo(9, "Auto", "Mclaren", "PENDIENTE", "Blanco", "2022", "Elegancia y estilo", 175);
 
 
-/*Motos*/ 
+    /*MOTOS*/
+    crearVehiculo(10, "Moto", "Yamaha", "R6", "Blanco", "2024", "Rapidez al instante", 75);
+    crearVehiculo(11, "Moto", "Kawasaki", "Nija H2R", "Blanco", "2019", "Confort y elegancia", 95);
+    crearVehiculo(12, "Moto", "Ducati", "PENDIENTE", "Blanco", "2025", "Diseño y calidad", 125);
+    crearVehiculo(13, "Moto", "Yamaha", "MT 09", "Blanco", "2020", "Eficiencia de combustible", 100);
+    crearVehiculo(14, "Moto", "Yamaha", "R1", "Blanco", "2017", "Rapidez al instante", 135);
+    crearVehiculo(15, "Moto", "Yamaha", "MT 15", "Blanco", "2020", "Diseño y calidad", 95);
+}
+
+/**
+ * Función que por parámetros recibe los datos de los vehículos para luego instanciar los objetos y agregarlos todos a un array
+ */
+function crearVehiculo(id, tipo, marca, modelo, color, anio, descripcion, precio) {
+    const objVehiculo = new Vehiculo(id, tipo, marca, modelo, color, anio, descripcion, precio);
+    vehiculos.push(objVehiculo);
+}
+
+crearObjetoVehiculo();
+
+/*
+const inhabilitar = vehiculos.find(auto => auto.id = 10);
+
+if (inhabilitar){
+    inhabilitar.modificarDisponibilidad(false);
+}
+*/
+
+console.log(vehiculos);
+
